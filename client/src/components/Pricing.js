931fd46af5b476fcdaa81dc029a7836eebd5b749
Yashvardhan Arora <yash22arora@gmail.com>
2022-12-06 03:34:58 +0530

    Add media query for pricing
    Signed-off-by: Yashvardhan Arora <yash22arora@gmail.com>

--- client/src/components/Pricing.js
+++ client/src/components/Pricing.js
@@ -2,7 +2,7 @@ import PricingCard from "./PricingCard";
 
 const Pricing = () => {
   return (
-    <div className="w-full flex flex-row justify-between items-start px-64 mt-32 mb-20">
+    <div className="w-full flex flex-row justify-between items-start 2xl:px-64 sm:px-8 mt-32 mb-20">
       <PricingCard />
       <PricingCard />
       <PricingCard />
