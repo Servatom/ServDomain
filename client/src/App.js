efad0ffd26ea104a38caaa846416e6102e1e8865
Yashvardhan Arora <yash22arora@gmail.com>
2022-12-05 21:51:28 +0530

    Create hero component
    Signed-off-by: Yashvardhan Arora <yash22arora@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
-function App() {
-    return (
-        <div>
-        </div>
-    );
-    }
+import bgFlare from "./assets/media/flare_light.avif";
+import Hero from "./components/Hero";
 
-    export default App;
-    
+function App() {
+  return (
+    <div className="w-full h-full min-h-screen flex flex-col text-gray-300 items-center px-40">
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Hero.js
@@ -0,0 +1,12 @@
+const Hero = () => {
+  return (
+    <div className="select-none">
+      <h1 className="font-semibold text-8xl mt-32">servdomain.</h1>
+      <p className="text-center mt-4 opacity-50 text-xl">
+        Flexible Subdomain Plans
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
