9573c788fc6af89c5e81ca72d3891faa6059a0e0
Yashvardhan Arora <yash22arora@gmail.com>
2022-12-06 18:59:27 +0530

    Convert pricing layout to grid
    Signed-off-by: Yashvardhan Arora <yash22arora@gmail.com>

--- client/src/components/Pricing/Pricing.js
+++ client/src/components/Pricing/Pricing.js
@@ -3,7 +3,7 @@ import { pricings } from "../../constants/pricing";
 
 const Pricing = () => {
   return (
-    <div className="w-full flex flex-row justify-between items-start 2xl:px-64 sm:px-8 mt-32 mb-20">
+    <div className="w-full grid grid-cols-3 gap-8 justify-around max-w-screen-xl items-start mt-32 mb-20">
       {pricings.map((pricing) => (
         <PricingCard key={pricing.id} pricing={pricing} />
       ))}
