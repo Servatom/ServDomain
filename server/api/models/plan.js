d65f85cfd5622f64618c4e4d5003cf6ce5ffc02f
Yashvardhan Arora <yash22arora@gmail.com>
2024-05-20 02:00:11 +0530

    Add Plan Schema

---
+++ server/api/models/plan.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const planSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  ownerID: { type: String, required: true, ref: "User" },
+  domainID: { type: String, required: true, ref: "Domain" },
+  planType: { type: String, required: true },
+  expiry: { type: Date, required: true },
+  stripeSubscriptionId: { type: String, required: true, default: "x" },
+  records: { type: Array, default: [] },
+  created_at: { type: Date, default: Date.now },
+});
+
+const Plan = mongoose.model("Plan", planSchema);
+
+module.exports = { Plan };
--- server/api/models/record.js
+++ server/api/models/record.js
@@ -5,13 +5,14 @@ const recordSchema = mongoose.Schema({
   cloudflareId: { type: String, required: true, default: "x" },
   cloudflareZoneId: { type: String, required: true, default: "x" },
   domainID: { type: String, required: true, ref: "Domain" },
-  stripeSubscriptionId: { type: String, required: true, default: "x" },
+  planId: { type: String, required: true, ref: "Plan" },
+  stripeSubscriptionId: { type: String, required: true, default: "x" }, //TODO: remove this
   ownerID: { type: String, required: true, ref: "User" },
   name: { type: String, required: true, unique: true },
   content: { type: String, required: true },
   type: { type: String, required: true },
-  plan: { type: String, required: true },
-  expiry: { type: Date, required: true },
+  plan: { type: String, required: true }, // TODO: remove this
+  expiry: { type: Date, required: true }, // TODO: remove this
   status: { type: String, default: "processing" },
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now },
